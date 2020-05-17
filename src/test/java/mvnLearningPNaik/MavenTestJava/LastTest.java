2e4c979b1e1cf4cafed66012150d2566f7a32975
Prashant <prashantnaik731@gmail.com>
2020-05-17 12:14:51 +0100

    last commit to master

---
+++ src/test/java/mvnLearningPNaik/MavenTestJava/LastTest.java
@@ -0,0 +1,11 @@
+package mvnLearningPNaik.MavenTestJava;
+
+import org.testng.annotations.Test;
+
+public class LastTest {
+	@Test
+	public void lastTest()
+	{
+		System.out.println("lastTest");
+	}
+}
